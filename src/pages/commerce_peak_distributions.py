dc906f16ebb9334fd3754fe694bae769e9f806b7
sjtalkar <sjtalkar@live.com>
2023-02-27 12:15:43 -0800

    Trying a path change by including root for render

--- src/pages/commerce_peak_distributions.py
+++ src/pages/commerce_peak_distributions.py
@@ -22,7 +22,7 @@ dash.register_page(__name__, title='Commercial and Non-commercial Expedition Dis
                    name='Commercial and Non-commercial Expedition Distributions')
 
 # print(f"This is the current directory : {os.path.abspath(os.getcwd())}")
-peak_exped_df = pd.read_csv(os.path.join("src", "data", "dash", "peak_exped_df.csv"), dtype={'COMRTE':object})
+peak_exped_df = pd.read_csv(os.path.join("capstone_dashboard", "src", "data", "dash", "peak_exped_df.csv"), dtype={'COMRTE':object})
 #These are generated every time page is loaded
 exped_commercial_type_df = peak_exped_df[~peak_exped_df['COMRTE'].isna()].copy()
 
